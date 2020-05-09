6c9903a204581ca35bb0267a574946a7e6c20fab
Greg Nicholson <greg@theme44.com>
2020-05-09 01:16:47 +0100

    Reworked intro and animations

--- components/Intro/Intro.vue
+++ components/Intro/Intro.vue
@@ -1,9 +1,8 @@
 <template>
-  <section class="w-screen h-screen overflow-hidden bg-primary">
+  <section class="w-screen h-screen overflow-hidden">
+    <div class="cover" />
     <div class="intro-loader text-white">
-      <span class="intro-loader-x">X</span>
-      <span class="intro-loader-y">Y</span>
-      <span class="intro-loader-z">Z</span>
+      Front End Developer.
     </div>
   </section>
 </template>
@@ -12,50 +11,27 @@
   import { gsap } from 'gsap/all';
 
   export default {
-    data: () => ({
-      skills: [
-        'Vue',
-        'Nuxt',
-        'Vuex',
-        'Javascript',
-        'Graph QL',
-        'REST API\'s',
-        'WordPress',
-        'Headless',
-        'eCommerce',
-      ],
-    }),
     methods: {
       introTimeline() {
-        const introTimeline = gsap.timeline({ delay: 1 });
-        
-        introTimeline.to('.intro-loader-x', {
-          x: '-50vw',
-          duration: 3,
-          ease: 'slow(0.3, 0.7, false)',
-        }, 0);
-        introTimeline.to('.intro-loader-z', {
-          x: '50vw',
-          duration: 3,
-          ease: 'slow(0.3, 0.7, false)',
-        }, 0);
-        introTimeline.to('.intro-loader-y', {
-          scale: '100',
-          duration: 2,
-          ease: 'slow(0.3, 0.7, false)',
-        }, '-=1.5');
-        
-        setTimeout(() => {
-          this.$emit('intro-complete', true);
-        }, (introTimeline.totalDuration() * 1000) + 500);
-      },
-      skillColor() {
-        const colors = [
-          'text-pink',
-          'text-cream',
-          'text-sky',
-        ];
-        return colors[Math.floor(Math.random() * colors.length)];
+        const introTimeline = gsap.timeline({
+          delay: 1,
+          onComplete: () => {
+            this.$emit('intro-complete', true);
+          },
+        });
+        introTimeline.to('.cover', {
+          width: '100%',
+          duration: 0.8,
+          ease: 'slow(0.9, 0.7, false)',
+        });
+        introTimeline.set('.cover', {
+          left: 0,
+        });
+        introTimeline.to('.cover', {
+          width: 0,
+          duration: 0.8,
+          ease: 'slow(0.9, 0.7, false)',
+        });
       },
     },
     mounted() {
@@ -68,29 +44,11 @@
 
 <style lang="scss" scoped>
   .intro-loader {
-    @apply absolute top-0 left-0 w-screen h-screen flex items-center justify-center font-para font-bold;
-    font-size: 10vw;
-
-    span {
-      &:nth-child(2) {
-        margin-left: -0.01em;
-      }
-
-      &:nth-child(3) {
-        margin-left: -0.06em;
-      }
-    }
+    @apply absolute top-0 left-0 w-screen h-screen flex items-center justify-center font-heading font-bold uppercase;
+    font-size: 3vw;
   }
 
-  .skills-list {
-    @apply absolute flex flex-no-wrap uppercase font-heading font-bold text-primary opacity-75;
-    font-size: 5vw;
-    width: max-content;
-
-    li {
-      &:not(:last-child) {
-        @apply mr-5;
-      }
-    }
+  .cover {
+    @apply absolute top-0 right-0 h-screen w-0 bg-primary;
   }
 </style>
